367dc729dc9b832ba4ff2c49341c2a7c384f1be3
Sam Llanwarne <errerr46@gmail.com>
2024-07-29 23:14:06 +0100

    there was an error with reset states of all projects

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,25 @@ class _ProjectGridState extends State<ProjectGrid> {
     Project('Project 5', 'assets/project5-pexels-eberhardgross-1366919.jpg', 'https://yourusername.github.io/project5'),
     Project('Project 6', 'assets/project6-pexels-eberhardgross-1624496.jpg', 'https://yourusername.github.io/project6'),
     Project('Project 7', 'assets/project7-pexels-pixabay-33045.jpg', 'https://yourusername.github.io/project7'),
+    Project('Project 8', 'assets/project8-pexels-pixabay-33109.jpg', 'https://yourusername.github.io/project8'),
+    Project('Project 9', 'assets/project9-pexels-pixabay-60597.jpg', 'https://yourusername.github.io/project9'),
+    Project('Project 10', 'assets/project10-pexels-pixabay-326055.jpg', 'https://yourusername.github.io/project10'),
+    Project('Project 11', 'assets/project11-pexels-sohi-807598.jpg', 'https://yourusername.github.io/project11'),
+    Project('Project 12', 'assets/project12-pexels-thatguycraig000-1563355.jpg', 'https://yourusername.github.io/project12'),
+    Project('Project 13', 'assets/project13-pexels-vladalex94-1402787.jpg', 'https://yourusername.github.io/project13'),
+    Project('Project 14', 'assets/project1-nice-clear-light-bulbs-eujn90ms9da1bw9j.jpg', 'https://yourusername.github.io/project1'),
+    Project('Project 15', 'assets/project2-3cb45f6e59190e8213ce0a35394d0e11-nice.jpg', 'https://yourusername.github.io/project2'),
+    Project('Project 16', 'assets/project3-pexels-eberhardgross-443446.jpg', 'https://yourusername.github.io/project3'),
+    Project('Project 17', 'assets/project4-pexels-eberhardgross-443446.jpg', 'https://yourusername.github.io/project4'),
+    Project('Project 18', 'assets/project5-pexels-eberhardgross-1366919.jpg', 'https://yourusername.github.io/project5'),
+    Project('Project 19', 'assets/project6-pexels-eberhardgross-1624496.jpg', 'https://yourusername.github.io/project6'),
+    Project('Project 20', 'assets/project7-pexels-pixabay-33045.jpg', 'https://yourusername.github.io/project7'),
+    Project('Project 21', 'assets/project8-pexels-pixabay-33109.jpg', 'https://yourusername.github.io/project8'),
+    Project('Project 22', 'assets/project9-pexels-pixabay-60597.jpg', 'https://yourusername.github.io/project9'),
+    Project('Project 23', 'assets/project10-pexels-pixabay-326055.jpg', 'https://yourusername.github.io/project10'),
+    Project('Project 24', 'assets/project11-pexels-sohi-807598.jpg', 'https://yourusername.github.io/project11'),
+    Project('Project 25', 'assets/project12-pexels-thatguycraig000-1563355.jpg', 'https://yourusername.github.io/project12'),
+    Project('Project 26', 'assets/project13-pexels-vladalex94-1402787.jpg', 'https://yourusername.github.io/project13'),
     // Add more projects here
   ];
 
@@ -90,14 +109,11 @@ class _ProjectGridState extends State<ProjectGrid> {
   }
 
   void resetAllProjects() {
-    for (final element in context.findRenderObject()!.visitChildren((child) {
-      final state = child as RenderObjectElement;
-      final widget = state.widget;
-      if (widget is ProjectWidget) {
-        final projectWidgetState = widget.key!.currentState as ProjectWidgetState;
-        projectWidgetState.reset();
+    context.visitChildElements((element) {
+      if (element is StatefulElement && element.state is ProjectWidgetState) {
+        (element.state as ProjectWidgetState).reset();
       }
-    })) {}
+    });
   }
 }
 
@@ -174,7 +190,6 @@ class ProjectWidgetState extends State<ProjectWidget> {
     }
   }
 }
-
 
 
 // class _ProjectGridState extends State<ProjectGrid> {
