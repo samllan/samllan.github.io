d1a384b90409f9b3db8a574bd90117505798be5c
Sam Llanwarne <errerr46@gmail.com>
2024-07-29 17:59:52 +0100

    again, still trying to find the one and way which works

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -24,29 +23,9 @@ class MyApp extends StatelessWidget {
 class PortfolioPage extends StatelessWidget {
   const PortfolioPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Portfolio - Samuel Llanwarne'),
-      ),
-      body: ProjectGrid(),
-    );
-  }
-}
-
-class Project {
-  final String name;
-  final String imageUrl;
-  final String projectUrl;
-
-  const Project(this.name, this.imageUrl, this.projectUrl);
-}
-
-class ProjectGrid extends StatelessWidget {
   final List<Project> projects = const [
-    Project('Project 1', 'assets/project1-nice-clear-light-bulbs-eujn90ms9da1bw9j.jpg', 'https://yourusername.github.io/project1'),
-    Project('Project 2', 'assets/project2-3cb45f6e59190e8213ce0a35394d0e11-nice.jpg', 'https://yourusername.github.io/project2'),
+    Project('Project 1', 'assets/project1.png', 'https://github.com/yourusername/project1'),
+    Project('Project 2', 'assets/project2.png', 'https://github.com/yourusername/project2'),
     Project('Project 3', 'assets/project3-pexels-eberhardgross-443446.jpg', 'https://yourusername.github.io/project3'),
     Project('Project 4', 'assets/project4-pexels-eberhardgross-443446.jpg', 'https://yourusername.github.io/project4'),
     Project('Project 5', 'assets/project5-pexels-eberhardgross-1366919.jpg', 'https://yourusername.github.io/project5'),
@@ -68,26 +47,35 @@ class ProjectGrid extends StatelessWidget {
     // Add more projects here
   ];
 
-  ProjectGrid({super.key}) {
-    projects.shuffle(Random());
-  }
-
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      padding: const EdgeInsets.all(16.0),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // 3 tiles per row
-        crossAxisSpacing: 16.0, // horizontal spacing
-        mainAxisSpacing: 16.0, // vertical spacing
-        childAspectRatio: 1.0, // aspect ratio to make them square and to control height
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Portfolio - Samuel Llanwarne'),
       ),
-      itemCount: projects.length,
-      itemBuilder: (context, index) {
-        return ProjectWidget(project: projects[index]);
-      },
+      body: GridView.builder(
+        padding: const EdgeInsets.all(16.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, // 3 tiles per row
+          crossAxisSpacing: 16.0, // horizontal spacing
+          mainAxisSpacing: 16.0, // vertical spacing
+          childAspectRatio: 1.0, // aspect ratio to make them square
+        ),
+        itemCount: projects.length,
+        itemBuilder: (context, index) {
+          return ProjectWidget(project: projects[index]);
+        },
+      ),
     );
   }
+}
+
+class Project {
+  final String name;
+  final String imageUrl;
+  final String projectUrl;
+
+  const Project(this.name, this.imageUrl, this.projectUrl);
 }
 
 class ProjectWidget extends StatefulWidget {
@@ -109,26 +97,27 @@ class ProjectWidgetState extends State<ProjectWidget> {
       onExit: (_) => setState(() => isHovered = false),
       child: GestureDetector(
         onTap: () => _launchURL(widget.project.projectUrl),
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(widget.project.imageUrl),
-              fit: BoxFit.cover,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                widget.project.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
-            borderRadius: BorderRadius.circular(8.0), // Optional: Adds rounded corners
-          ),
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 200),
-            color: isHovered ? Colors.black54 : Colors.transparent,
-            child: Center(
-              child: isHovered
-                  ? Text(
-                      widget.project.name,
-                      style: const TextStyle(color: Colors.white, fontSize: 24),
-                    )
-                  : Container(),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              color: isHovered ? Colors.black54 : Colors.transparent,
+              child: Center(
+                child: isHovered
+                    ? Text(
+                        widget.project.name,
+                        style: const TextStyle(color: Colors.white, fontSize: 24),
+                      )
+                    : Container(),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
